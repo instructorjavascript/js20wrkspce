e0c0eaa0be3b18faca49901af54dbea4cfdea01c
instructorjavascript <instructorjavascript@gmail.com>
2020-01-30 12:28:28 -0600

    created simple website with all resources

---
+++ strings.js
@@ -0,0 +1,9 @@
+function help(){
+    document.getElementById("demo").innerHTML = "Thanks For Helping US";
+    alert("Thank You For Saving US , A Shark!! Was Going To EAT All OF US ALIVE !!!!");
+}
+
+function dontHelp(){
+    alert("5 people were eaten by a Baby Shark , and the Momma Shark watched it all. Thanks for Nothing");
+    document.getElementById("demo").innerHTML = "Why Wont You HELP US !!!!!"
+}
