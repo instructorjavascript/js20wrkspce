6ac4b51c63adeb72b234e3fc80c801c3372def50
instructorjavascript <instructorjavascript@gmail.com>
2020-02-10 13:37:51 -0600

    Operators

---
+++ arithmetic.js
@@ -0,0 +1,21 @@
+var num1 = 10;
+var num2 = 30;
+
+// addition +
+console.log( "Number 1 is equal to: " + num1 + "  Number 2 is equal to: " + num2 + "  The result of " + num1 + " + " + num2 + " is: " + (num1 + num2))
+
+// subtraction - 
+console.log( "Number 1 is equal to: " + num1 + "  Number 2 is equal to: " + num2 + "  The result of " + num1 + " - " + num2 + " is: " + (num1 - num2))
+
+// multiplication
+console.log( "Number 1 is equal to: " + num1 + "  Number 2 is equal to: " + num2 + "  The result of " + num1 + " * " + num2 + " is: " + (num1 * num2))
+
+//console.log( " hey " + ++num1);
+//console.log( " hey " + ++num1);
+console.log( " decrement " + --num1);
+console.log( " hey " + num1++);
+console.log( " hey " + --num1);
+
+
+
+ 
---
+++ relational.js
@@ -0,0 +1,4 @@
+// relational 
+
+// equals = =
+
